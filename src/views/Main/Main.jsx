3b170d6e854c45c4a8d5b9ccb81d0de2e6017896
aniqmir <aniqmir@gmail.com>
2019-02-05 19:27:20 +0500

    playnow

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -2,29 +2,30 @@ import React, { Component } from "react";
 
 import {
     Route,
-    NavLink,
+    // NavLink,
     HashRouter
   } from "react-router-dom";
-  import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem 
-          } 
-  from 'reactstrap';
+  // import {
+  //   Collapse,
+  //   Navbar,
+  //   NavbarToggler,
+  //   NavbarBrand,
+  //   Nav,
+  //   NavItem,
+  //   UncontrolledDropdown,
+  //   DropdownToggle,
+  //   DropdownMenu,
+  //   DropdownItem 
+  //         } 
+  // from 'reactstrap';
   import Home from '../Home/Home.jsx';
-  import DegenPlatform from '../Section1/Section1.jsx';
-  import Ante from "../Section2/Section2.jsx";
-  import SuperRepresentative from "../Section3/Section3.jsx";
-  import RoadMap from '../Section4/Section4.jsx';
-  import Partners from '../Section5/Section5.jsx';
-  import Footer from '../Footer/Footer.jsx';
+  // import DegenPlatform from '../Section1/Section1.jsx';
+  // import Ante from "../Section2/Section2.jsx";
+  // import SuperRepresentative from "../Section3/Section3.jsx";
+  // import RoadMap from '../Section4/Section4.jsx';
+  // import Partners from '../Section5/Section5.jsx';
+  // import Footer from '../Footer/Footer.jsx';
+  import PlayNow from '../PlayNow/Drawer/Drawer.jsx'
 
 
 //   import Grid from '@material-ui/core/Grid';
@@ -48,27 +49,29 @@ import {
       return (
         <HashRouter>
           <div>
-                <Navbar color="dark" dark expand="md">
+                {/* <Navbar color="dark" dark expand="md">
                 <NavbarBrand href="/">DEGENGAMES</NavbarBrand>
                 <NavbarToggler onClick={this.toggle} />
                 <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="container" navbar>
-                    <NavItem>
-                    <NavLink to="/degenplatform" style={{color:'white',marginRight:'2%'}}>Degen Platform</NavLink>
+                    <Nav className="container" navbar >
+                    <NavItem style={{paddingTop:'2%'}}>
+                    <NavLink to="/degenplatform" style={{color:'white',marginRight:'2%'}}><h5>Degen Platform</h5></NavLink>
                     </NavItem>
-                    <NavItem style={{marginRight:'1%',marginLeft:'1%'}}>
-                    <NavLink to="/ante" style={{color:'white'}}>ANTE</NavLink>
+                    <NavItem style={{marginRight:'1%',marginLeft:'1%',paddingTop:'2%'}}>
+                    <NavLink to="/ante" style={{color:'white'}}><h5>ANTE</h5></NavLink>
                     </NavItem>
-                    <NavItem style={{marginRight:'2%'}}>
-                    <NavLink to="/superrepresentative" style={{color:'white'}}>Super Representative</NavLink>
+                    <NavItem style={{marginRight:'2%',paddingTop:'2%'}}>
+                    <NavLink to="/superrepresentative" style={{color:'white'}}><h5>Super Representative</h5></NavLink>
                     </NavItem>
-                    <NavItem style={{marginRight:'2%'}}>
-                    <NavLink to="/roadmap" style={{color:'white'}}>Road Map</NavLink>
+                    <NavItem style={{marginRight:'2%',paddingTop:'2%'}}>
+                    <NavLink to="/roadmap" style={{color:'white'}}><h5>Road Map</h5></NavLink>
                     </NavItem>
-                    <NavItem style={{marginRight:'2%'}}>
-                    <NavLink to="/partners" style={{color:'white'}}>Partners</NavLink>
+                    <NavItem style={{marginRight:'2%',paddingTop:'2%'}}>
+                    <NavLink to="/partners" style={{color:'white'}}><h5>Partners</h5></NavLink>
                     </NavItem>
-
+                    <NavItem style={{marginRight:'2%',paddingTop:'2%'}}>
+                    <NavLink to="/playnow" style={{color:'white'}}><h5>Play Now</h5></NavLink>
+                    </NavItem>
                     <UncontrolledDropdown nav inNavbar style={{marginLeft:'15%'}}>
                         <DropdownToggle nav caret>
                         Options
@@ -89,26 +92,17 @@ import {
 
                     </Nav>
                 </Collapse>
-                </Navbar>
-            {/* <Grid item container spacing={0}>
-                <Grid item xs={12}>
-                    <Home/>
-                    <DegenPlatform/>
-                    <Ante/>
-                    <SuperRepresentative/>
-                    <RoadMap/>
-                    <Partners/>
-                </Grid>
-            </Grid> */}
+                </Navbar> */}
             <div>
             <Route exact path="/" component={Home}/>
-            <Route path="/degenplatform" component={DegenPlatform}/>
+            {/* <Route path="/degenplatform" component={DegenPlatform}/>
             <Route path="/ante" component={Ante}/>
             <Route path="/superrepresentative" component={SuperRepresentative}/>
             <Route path="/roadmap" component={RoadMap}/>
-            <Route path="/partners" component={Partners}/>
+            <Route path="/partners" component={Partners}/> */}
+            <Route path="/playnow" component={PlayNow}/>
             </div>
-            <Footer/>
+            {/* <Footer/> */}
           </div>
         </HashRouter>
       );
---
+++ src/views/PlayNow/Drawer/DrawerTabs/Messages/Messages.jsx
@@ -0,0 +1,331 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: '100%',
+    backgroundColor: '#181818',
+    color:'white',
+    overflow:'auto'
+  },
+  inline: {
+    display: 'inline',
+    color:'white'
+  },
+});
+
+function AlignItemsList(props) {
+  const { classes } = props;
+  return (
+    <List className={classes.root}>
+    <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>
+      <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>   <ListItem alignItems="flex-start">
+        <ListItemText
+        className={classes.textcolor}
+          primary={
+            <Typography component="span" className={classes.inline} color="inherit">
+              Level Here:
+            </Typography>
+        }
+          secondary={
+              <Typography component="span" className={classes.inline} color="inherit">
+                Message Here
+              </Typography>
+          }
+        />
+      </ListItem>
+    </List>
+  );
+}
+
+AlignItemsList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AlignItemsList);
